5dc533e29f229a208ee24274519a5d2ea712335b
ZainNazeer30 <ranazainrzn31@gmail.com>
2025-09-28 00:03:51 +0500

    mistake

--- backend/pf_backend/folio/views.py
+++
@@ -1,8 +0,0 @@
-# folio/views.py
-from rest_framework import generics
-from .models import ContactMessage
-from .serializer import ContactMessageSerializer
-
-class ContactView(generics.ListCreateAPIView):
-    queryset = ContactMessage.objects.all()
-    serializer_class = ContactMessageSerializer
